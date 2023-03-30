b974228fa7f5d3f9a603cf0091064ee51e77f336
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-03-30 13:57:30 -0500

    Create searchbar.js

---
+++ js/searchbar.js
@@ -0,0 +1,26 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal_container');
+
+open.addEventListener('click', () => {
+	modal.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+	modal.classList.remove('show');
+});
+// JavaScript code
+function search_game() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('games');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
