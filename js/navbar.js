f07c8eefcba32c6a2e47e8d5dea32deaf531b09a
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-04-20 10:05:57 -0500

    sda

--- js/navbar.js
+++ js/navbar.js
@@ -1,44 +1,17 @@
-const body = document.querySelector("body"),
-    nav = document.querySelector("nav"),
-    modeToggle = document.querySelector(".dark-light"),
-    searchToggle = document.querySelector(".searchToggle"),
-    sidebarOpen = document.querySelector(".sidebarOpen"),
-    siderbarClose = document.querySelector(".siderbarClose");
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {sticky()};
 
-    let getMode = localStorage.getItem("mode");
-        if(getMode && getMode === "dark-mode"){
-        body.classList.add("dark");
-        }
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-// js code to toggle dark and light mode
-    modeToggle.addEventListener("click" , () =>{
-    modeToggle.classList.toggle("active");
-    body.classList.toggle("dark");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-    // js code to keep user selected mode even page refresh or file reopen
-    if(!body.classList.contains("dark")){
-        localStorage.setItem("mode" , "light-mode");
-    }else{
-        localStorage.setItem("mode" , "dark-mode");
-    }
-    });
-
-// js code to toggle search box
-    searchToggle.addEventListener("click" , () =>{
-    searchToggle.classList.toggle("active");
-    });
-
-        
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
-    nav.classList.add("active");
-});
-
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
-
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-        nav.classList.remove("active");
-    }
-});
-
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function sticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
