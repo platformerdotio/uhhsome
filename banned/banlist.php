dae33ae76cbef08398fea8086f6e98c0d4abd12b
krazykoolkat <120543332+krazykoolkat@users.noreply.github.com>
2023-04-24 14:01:51 -0500

    Update banlist.php

--- banned/banlist.php
+++ banned/banlist.php
@@ -1,4 +1,9 @@
-<?php
-  session_start();
-  require('http://platformerdotio.github.io/banned/banlist.php');
+<?php 
+
+	$deny = array("10.115.41.189"); // To add more IP's array("127.0.0.1", "127.0.0.2", "127.0.0.3")
+	if (in_array ($_SERVER['REMOTE_ADDR'], $deny)) {
+	   header("location: http://platformerdotio.github.io/banned/"); // Page to redirect banned users to (/banned/ by default)
+	   exit();
+	}
+
 ?>
