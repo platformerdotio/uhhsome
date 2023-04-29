ed01fe294320b4ea7fdc9e5e9b8df3808ef9bc15
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-04-29 13:35:43 -0500

    sd

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,37 +1,3 @@
-
-let url = window.location.pathname;
-
-let pageurl = "";
-
-if (url.indexOf("html") > -1) {
-  pageurl = url.match(/([^\/]+)(?=\.\w+$)/)[0];
-} else {
-  pageurl = url.substring(url.lastIndexOf("/") + 1, url.length);
-}
-
-try {
-  const found = gamesArr.find((element) => element.link == pageurl);
-
-  const content_title = document.querySelector("#content-title");
-  content_title.innerText = found.name;
-  const content_creator = document.querySelector("#content-creator");
-  content_creator.innerText = "by " + found.developer; //set the info
-  const contentdesc = document.querySelector("#content-desc");
-  contentdesc.innerText = found.desc;
-
-  const contentcontrols = document.querySelector("#content-controls");
-  for (let i = 0; i < found.controls.length; i++) {
-    let controlitem = document.createElement("li");
-    controlitem.innerText = found.controls[i];
-    contentcontrols.appendChild(controlitem);
-  }
-} catch (err) {
-  console.log(err);
-}
-
-
-
-//Fullscreen function
 const reqFs = (elem) => {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
@@ -44,26 +10,7 @@ const reqFs = (elem) => {
   }
 };
 
-//iframe fullscreen
 const iFullscreen = () => {
   const elem = document.getElementsByTagName("iframe")[0];
-  reqFs(elem);
-};
-
-//Canvas fullscreen
-const cFullscreen = () => {
-  const elem = document.getElementById("canvas");
-  reqFs(elem);
-};
-
-//Ruffle fullscreen
-const rFullscreen = () => {
-  const elem = document.getElementById("player");
-  reqFs(elem);
-};
-
-//Emulator fullscreen
-const eFullscreen = () => {
-  const elem = document.getElementById("game");
   reqFs(elem);
 };
